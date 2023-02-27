c82167d999fabc3ff690696510d86947611a39f4
DenisZaviriukha <dv.zavirukha@gmail.com>
2023-02-27 03:55:24 +0200

    fix 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,26 +6,25 @@ const message = document.querySelector('.feedback-form textarea')
 const STORAGE_KEY = "feedback-form-state"
 
 fillForm()
+
 function fillForm() {
   const faceControl  = localStorage.getItem(STORAGE_KEY)
   if (JSON.parse(faceControl)) {
-      b = JSON.parse(faceControl)
-      email.value = b.email
-      message.value = b.message 
-      return
-  }
+    b = JSON.parse(faceControl)
+    email.value = b.email
+    message.value = b.message
+  } else {
     email.value = ''
     message.value = ''
-    return
+  }
 }
 
 function onFormInput () { 
-  dataArray = {
+  let dataArray = {
     email: email.value,
     message: message.value
   }
   localStorage.setItem(STORAGE_KEY, JSON.stringify(dataArray))
-  console.log(localStorage.getItem(STORAGE_KEY))
 }
 
 const onSubmit = (e) => {
@@ -38,31 +37,6 @@ const onSubmit = (e) => {
   }
   return alert('fill hte inputs')
 }
+
 form.addEventListener('input', throttle(onFormInput, 500))
 form.addEventListener('submit', onSubmit)
-
-
-
-// function updateOutput() {
-//   form.elements.value = localStorage.getItem(STORAGE_KEY) || "";
-// }
-
-// updateOutput()
-// let dataArray = {
-//   email: '',
-//   message: ''
-// }
-// const onFormInput = (e) => {
-//     e.preventDefault()
-//    if (e.target.name === 'email') {
-//         dataArray.email = e.target.value
-//         localStorage.setItem( STORAGE_KEY, JSON.stringify(dataArray.name)) 
-//         console.log(localStorage.getItem(STORAGE_KEY))
-//     }
-//     if (e.target.name === 'message') {
-//         dataArray.message = e.target.value
-//         localStorage.setItem(STORAGE_KEY, JSON.stringify(dataArray))
-//         // e.target.value = localStorage.getItem("feedback-form-state")
-//         console.log(localStorage.getItem(STORAGE_KEY))
-//     }
-// }
